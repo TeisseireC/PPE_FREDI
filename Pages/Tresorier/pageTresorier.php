c42fd74927f05bd82a937b4e91446b55483deddd
YannCecconato <cecconato.yann@gmail.com>
2018-11-05 14:51:06 +0100

    Ajout de pageTresorier.php, de tresorier.php, de tresorierDAO.php et modification de DAO.php

---
+++ Pages/Tresorier/pageTresorier.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+
+    <title>Accueil - Trésorier</title>
+    <!-- Style -->
+    <link rel="stylesheet" type="text/css" href="../../assets/css/styles.css"/> 
+  </head>
+
+  <body>
+    <!-- Start header -->
+    <header>
+        <?php
+          include "../../assets/include/menu2.php";
+        ?>
+    </header>
+    <!-- End header -->
+
+    <!-- Start section -->
+    <section>      
+        <p>Bienvenue sur le site de gestion des bordereaux, Trésorier.
+          Sélectionner "Bordereaux" pour accéder à tous les bordereaux des utilisateurs de la Ligue et 
+          vérifier l'état de chacun des bordereaux pour, par la suite, les valider ou les modifier. </p>
+    </section>
+    <!-- End section -->
+
+    <!-- Start footer -->
+    <footer>
+        <p>Site développé par Clément Bonnefont, Cyril Teisseire, Antoine Vucic et Yann Cecconato</p>                  
+    </footer>
+    <!-- End footer -->
+                      
+  </body>
+</html>
