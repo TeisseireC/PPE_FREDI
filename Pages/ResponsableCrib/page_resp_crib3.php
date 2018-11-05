695afbd02e9b4489fdbea74f3175c0ed2b75cd5d
CBonnefont <clement.bonnefont1@gmail.com>
2018-11-05 16:56:55 +0100

    Ajout des pages concernant les responsables crib

---
+++ Pages/ResponsableCrib/page_resp_crib3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+
+    <title>Accueil - Trésorier</title>
+    <!-- Style -->
+    <link rel="stylesheet" type="text/css" href="../../assets/css/styles.css"/> 
+  </head>
+
+  <body>
+    <!-- Start header -->
+    <header>
+        <?php
+          include "../../assets/include/menu2.php";
+        ?>
+    </header>
+    <!-- End header -->
+
+    <!-- Start section -->
+    <section>      
+        <p>Bienvenue sur la page de gestion "Motifs de frais"</p>
+          
+    </section>
+    <!-- End section -->
+
+    <!-- Start footer -->
+    <footer>
+        <p>Site développé par Clément Bonnefont, Cyril Teisseire, Antoine Vucic et Yann Cecconato</p>                  
+    </footer>
+    <!-- End footer -->
+                      
+  </body>
+</html>
