af32e4f8caccfa4481568cb28d778f8d5d19c956
CBonnefont <clement.bonnefont1@gmail.com>
2018-12-03 16:54:53 +0100

    Ajout login du tresorier

--- assets/Database/insertion_resp_crib.sql
+++ assets/Database/insertion_resp_crib.sql
@@ -3,3 +3,5 @@ VALUES
 (1, "Jacques", "Célert", "jacques.celert@yahoo.fr", 1, "$2a$10$D/n4xF3hmy7mypYpYQr/fuEd1oFuCYOnBRZA0/AF7J9k7Znk1AUoO"),
 (2, "Lucie", "Ditée", "lucie.ditee@gmail.com", 2, "$2a$10$iHPVDfG9tQ76W4bfluDSIe4un49HHDjOo8gdMblicjLrRUdLYIQ6m"),
 (3, "Sam", "Gratte", "sam.gratte@orange.fr", 3, "$2a$10$F4LXepwApnJcV9M8khLt5.prP6rzCc8U07SUl2khUnsMMxU4A/9Te");
+
+-- MDP : azer1234 --
